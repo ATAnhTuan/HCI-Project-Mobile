2f88d333c16468743098397ac5d071d4e461166e
AnhTuanLazy <anhtuan.itwork@gmail.com>
2022-06-22 08:49:36 +0700

    update home

--- HCI-Project-Mobile/lib/Screen/main.dart
+++ HCI-Project-Mobile/lib/Screen/main.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'Home/HomeScreen.dart';
 import 'package:material_color_utilities/material_color_utilities.dart';
+import 'Home/BottomBar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,75 +15,75 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyStatefulWidget(),
+      home: BottomBar(),
     );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
+// class BottomBar extends StatefulWidget {
+//   const BottomBar({Key? key}) : super(key: key);
 
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
+//   @override
+//   State<BottomBar> createState() => _MyStatefulWidgetState();
+// }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-   HomeScreen(),
-    Text(
-      'Index 1: Chat',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Search',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: Profile',
-      style: optionStyle,
-    )
-  ];
+// class _MyStatefulWidgetState extends State<BottomBar> {
+//   int _selectedIndex = 0;
+//   static const TextStyle optionStyle =
+//       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+//   static const List<Widget> _widgetOptions = <Widget>[
+//    HomeScreen(),
+//     Text(
+//       'Index 1: Chat',
+//       style: optionStyle,
+//     ),
+//     Text(
+//       'Index 2: Search',
+//       style: optionStyle,
+//     ),
+//     Text(
+//       'Index 3: Profile',
+//       style: optionStyle,
+//     )
+//   ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.green[400],
-        unselectedItemColor: Colors.white,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            label: 'Trang Chủ',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat_bubble_outline),
-            label: 'Chat',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search_outlined),
-            label: 'Tìm kiếm',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outlined),
-            label: 'Thông Tin',
-          )
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.white,
-        onTap: _onItemTapped,
-      ),
-    );
-  }
-}
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: _widgetOptions.elementAt(_selectedIndex),
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         type: BottomNavigationBarType.fixed,
+//         backgroundColor: Colors.green[400],
+//         unselectedItemColor: Colors.white,
+//         items: const <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home_outlined),
+//             label: 'Trang Chủ',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.chat_bubble_outline),
+//             label: 'Chat',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.search_outlined),
+//             label: 'Tìm kiếm',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.person_outlined),
+//             label: 'Thông Tin',
+//           )
+//         ],
+//         currentIndex: _selectedIndex,
+//         selectedItemColor: Colors.white,
+//         onTap: _onItemTapped,
+//       ),
+//     );
+//   }
+// }
